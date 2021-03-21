89c01d2e93c9678f5ecf40580d52d6235018e7b2
Michael <michaeljhart760@gmail.com>
2021-03-21 07:10:52 -0700

    feat: add initial project with bash class

---
+++ Assets/Scripts/BashShell.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+/// <summary>
+/// Represents a bash shell.
+/// </summary>
+public class BashShell
+{
+    // member vars
+    private readonly Process process;
+    
+    // Construction / Destruction
+    public BashShell()
+    {
+        var bashExePath = GetBashExePath();
+        
+        process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = bashExePath,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+        
+        process.Start();
+        process.StandardInput.AutoFlush = true;
+    }
+    ~BashShell()
+    {
+        Dispose();
+    }
+    /// <summary>
+    /// Kills the bash process. If the bash process cannot be killed, returns false and does not throw
+    /// an error.
+    /// </summary>
+    /// <returns>True if process is killed, false if process unable to be killed (already killed)</returns>
+    public bool Dispose()
+    {
+        if (process == null || process.HasExited)
+            return false;
+        
+        process.Kill();
+        return true;
+    }
+    
+    // public methods
+    public string ExecuteCommand(string cmd)
+    {
+        // var escapedArgs = cmd.Replace("\"", "\\\"");
+        var streamWriter = process.StandardInput;
+        streamWriter.WriteLine(cmd);
+
+        return process.StandardOutput.ReadToEnd();
+    }
+
+    // private static methods
+    private static string GetBashExePath()
+    {
+        return Application.dataPath + "/Plugins/EditorTerminal/bash.exe";
+    }
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    private BashShell bashShell;
+
+    private void Awake()
+    {
+        bashShell = new BashShell();
+    }
+
+    private void OnDestroy()
+    {
+        bashShell.Dispose();
+    }
+    private void OnApplicationQuit()
+    {
+        bashShell.Dispose();
+    }
+}
